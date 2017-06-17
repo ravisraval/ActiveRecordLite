6be3d70334206bd691ecde9ec35e6e69a1eac68a
Ravi <raviraval@gmail.com>
2017-06-16 17:02:25 -0700

    End day commit, completed most of project!

--- lib/02_searchable.rb
+++ lib/02_searchable.rb
@@ -3,10 +3,24 @@ require_relative '01_sql_object'
 
 module Searchable
   def where(params)
-    # ...
+
+    where_input = params.map { |pair| "#{pair.first} = ?" }.join(" AND ")
+    attr_values = params.map { |pair| "#{pair.last}" }
+    
+    instance_params = DBConnection.execute(<<-SQL, *attr_values)
+      SELECT
+        *
+      FROM
+        #{table_name}
+      WHERE
+        #{where_input}
+    SQL
+
+    instance_params.map { |param| self.new(param)}
+
   end
 end
 
 class SQLObject
-  # Mixin Searchable here...
+  extend Searchable
 end
--- lib/03_associatable.rb
+++ lib/03_associatable.rb
@@ -10,29 +10,66 @@ class AssocOptions
   )
 
   def model_class
+    class_name.to_s.constantize
     # ...
   end
 
   def table_name
+    model_class.table_name
     # ...
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    # ...
+    options[:foreign_key] ||= "#{name.downcase}_id".to_sym
+    options[:primary_key] ||= :id
+    options[:class_name] ||= "#{name}".camelcase.singularize
+
+    @foreign_key = options[:foreign_key]
+    @primary_key = options[:primary_key]
+    @class_name = options[:class_name]
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    options[:foreign_key] ||= "#{self_class_name.downcase.singularize}_id".to_sym
+    options[:primary_key] ||= :id
+    options[:class_name] ||= "#{name}".camelcase.singularize
+
+    @foreign_key = options[:foreign_key]
+    @primary_key = options[:primary_key]
+    @class_name = options[:class_name]
   end
 end
 
 module Associatable
   # Phase IIIb
   def belongs_to(name, options = {})
+    for_key = DBConnection.execute(<<-SQL)
+      SELECT
+        #{model_class}.id
+      FROM
+        #{table_name}
+      WHERE
+        primary_key = self.id
+    SQL
+
+    newthingy = BelongsToOptions.new(name, options)
+    define_method(name) do
+      results = DBConnection.execute(<<-SQL, self.options[:class_name], for_key)
+        SELECT
+          *
+        FROM
+          ?.#{table_name}
+        WHERE
+          primary_key = ?
+      SQL
+
+      model_class.new(results.first)
+    end
+
     # ...
   end
 
@@ -46,5 +83,6 @@ module Associatable
 end
 
 class SQLObject
+  extend Associatable
   # Mixin Associatable here...
 end
